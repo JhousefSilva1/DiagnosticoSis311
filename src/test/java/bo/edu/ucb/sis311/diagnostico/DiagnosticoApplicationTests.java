4d932a91fd52f8abf0a11588c0e3a9af0abd623b
Jose Pozo Silva <jhosede23@gmail.com>
2023-08-14 18:57:54 -0400

    firsr

---
+++ src/test/java/bo/edu/ucb/sis311/diagnostico/DiagnosticoApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis311.diagnostico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
